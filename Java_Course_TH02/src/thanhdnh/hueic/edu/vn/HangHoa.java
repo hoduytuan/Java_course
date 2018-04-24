4c79c25ac07075c93114947ef379101475ac96d8
hoduytuan <32832027+hoduytuan@users.noreply.github.com>
2018-04-24 17:47:30 +0700

    Update HangHoa.java

--- Java_Course_TH02/src/thanhdnh/hueic/edu/vn/HangHoa.java
+++ Java_Course_TH02/src/thanhdnh/hueic/edu/vn/HangHoa.java
@@ -1,15 +1,62 @@
 package thanhdnh.hueic.edu.vn;
 
-public class HangHoa {
-	private String tenHang;
-	//Tiếp tục cho các thuộc tính còn lại
+lớp  công khai HangHoa {
+	private  string tenhang;
+	// Continue for relase properties
 
-	public String getTenHang() {
-		return tenHang;
+	 chuỗi tư nhân ngaysanxuat;
+	 dongia đôi riêng ;
+	private  int soluong;
+
+	public  String  getTenHang () {
+		return tenhang;
 	}
 
-	public void setTenHang(String tenHang) {
-		this.tenHang = tenHang;
+	public  void  setTenHang ( Chuỗi  th ) {
+		tenhang = th;
 	}
-	//Tiếp tục cho các phương thức còn lại
+	// Continue for remaining method
+
+	public  String  getNgaySanXuat () {
+		trả lại ngaysanxuat;
+	}
+
+	public  void  setNgaySanXuat ( Chuỗi  nsx ) {
+		ngaysanxuat = nsx;
+	}
+
+	công khai  double  getDonGia () {
+		trả lại dongia;
+	}
+
+	public  void  setDonGia ( double  dg ) {
+		dongia = dg;
+	}
+
+	public  int  getSoLuong () {
+		return soluong;
+	}
+
+	public  void  setSoLuong ( int  sl ) {
+		soluong = sl;
+	}
+
+	công khai  đôi  TinhTien () {
+		return dongia * soluong;
+	}
+
+	 HangHoa công cộng ( Chuỗi  thứ , Chuỗi  nsx , double  dg , int  sl ) {
+		tenhang = th;
+		ngaysanxuat = nsx;
+		dongia = dg;
+		soluong = sl;
+	}
+
+	 HangHoa công cộng () {
+	}
+
+	@Ghi đè
+	công khai  String  toString () {
+		return  " Tên hàng: "  + tenhang +  " \ n "  +  " Ngày sản xuất: "  + ngaysanxuat +  " \ n "  +
+				" Đơn giá: "  + dongia +  " \ n "  +  " Số lượng: "  + soluong +  " \ n "  +  " Thành tiền: "  + TinhTien (); }
 }
