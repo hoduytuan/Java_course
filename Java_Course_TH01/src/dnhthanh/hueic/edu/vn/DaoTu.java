7bb181313a96183353fa9c791e1ad70b3386f128
hoduytuan <32832027+hoduytuan@users.noreply.github.com>
2018-05-05 15:22:19 +0700

    Update DaoTu.java

--- Java_Course_TH01/src/dnhthanh/hueic/edu/vn/DaoTu.java
+++ Java_Course_TH01/src/dnhthanh/hueic/edu/vn/DaoTu.java
@@ -1,11 +1,19 @@
 package dnhthanh.hueic.edu.vn;
 
 public class DaoTu {
-	public static String thuchien(String s) {
-		String ns = new String();
-		//Viết nội dung xử lý xâu s cho ra xâu ns, trong đó ns là xâu đảo từ.
-		char[] a = s.toCharArray();
-		char[] b = new char[a.length];
-		return ns;
+	public static String thuchien(String str) {
+		int r = 0;
+		String a = " ";
+		for (int i = 0; str.length() > i; i++) 
+		{
+			if (str.substring(i, i + 1).compareTo(" ") == 0) 
+			{
+				a = str.substring(r, i) + " " + a;
+				r = i;
+			}
+		}
+		a = str.substring(r, str.length()) + " " + a;
+		a = a.substring(1);
+		return a.trim();
 	}
 }
